2b71225b862378c1615fc5b3e222b214d0f17f6d
ponnamandamohan <92107780+ponnamandamohan@users.noreply.github.com>
2021-11-19 21:29:12 +0530

    Add files via upload

--- range.py
+++ range.py
@@ -1,5 +1,4 @@
-start = int(input("Enter the start of range: "))
-end = int(input("Enter the end of range: "))
-for num in range(start, end + 1):
+list1 = [12,-7,5,64,-14]
+for num in list1:
     if num >= 0:
-        print(num, end = " ")
+       print(num, end = " ")
